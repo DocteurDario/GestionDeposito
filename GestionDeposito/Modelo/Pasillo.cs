d1d392453c6b12c692add33f332e8a41973faf44
DocteurDario <docteurdario@gmail.com>
2022-08-28 18:14:39 -0300

    Nueva clase pasillo

---
+++ GestionDeposito/Modelo/Pasillo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionDeposito.Modelo
+{
+    internal class Pasillo
+    {
+        public int IDPasillo { get; set; }  
+        public string Nombre { get; set; }
+        public int IDCliente { get; set; }   
+        public int IDContenedor { get; set; }   
+        public LadoPasillo LADOPASILLO { get; set; }
+        public bool Activo { get; set; }    
+
+    }
+
+    public enum LadoPasillo 
+    {
+        LadoA,
+        LaboB
+    }
+}
